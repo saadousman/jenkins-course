4a8db0fce7fb334a294580d105db626b7f88f68c
saadousman <62424985+saadousman@users.noreply.github.com>
2023-05-08 18:40:19 +0530

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -8,10 +8,10 @@ job('NodeJS Docker example') {
     triggers {
         scm('H/5 * * * *')
     }
-   /* wrappers {
+    wrappers {
         nodejs('nodejs') // this is the name of the NodeJS installation in 
                        // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
-    }  */
+    }  
     steps {
         dockerBuildAndPublish {
             repositoryName('sdousman/node-js-demo')
