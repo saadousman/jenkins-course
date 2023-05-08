589cdbc42c483552b84e71eabf1946a5d7131a0f
saadousman <62424985+saadousman@users.noreply.github.com>
2023-05-08 19:43:28 +0530

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -16,7 +16,7 @@ job('NodeJS Docker example') {
         dockerBuildAndPublish {
             repositoryName('sdousman/node-js-demo')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockercreds')
+            registryCredentials('dockers')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
